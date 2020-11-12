7f9ae3c936fabc9465b3c82c5f05e217f7981ef8
Irsyad Ashari <irsyad.ashari17@gmail.com>
2020-11-12 17:03:48 +0800

    adding dependency injection

--- Capstone SwiftUI/Core/Data/Locale/LocaleDataSource.swift
+++ Capstone SwiftUI/Core/Data/Locale/LocaleDataSource.swift
@@ -21,8 +21,6 @@ final class LocaleDataSource: NSObject {
     private let realm: Realm?
     private init(realm: Realm?) {
         self.realm = realm
-        
-       
     }
     static let sharedInstance: (Realm?) -> LocaleDataSource = {
         realmDatabase in return LocaleDataSource(realm: realmDatabase)
